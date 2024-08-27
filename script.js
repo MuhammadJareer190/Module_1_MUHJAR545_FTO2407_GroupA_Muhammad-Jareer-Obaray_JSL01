0f6b94e9c968d7ad6c2fd2a28c8c2ca861af57d0
Muhammad Jareer <jareerobaray@icloud.com>
2024-08-27 16:44:59 +0200

    Added the validation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function validateSyntax() {
+    let regEx = /^pet_[A-Z a-z 0-9]+$/;
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
