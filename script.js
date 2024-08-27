f7cf86ec3f45b3277f52fad639e679041565b63a
Muhammad Jareer <jareerobaray@icloud.com>
2024-08-27 16:46:37 +0200

    Added the principles to the validation

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+    if (regEx.test(input)) {
+        result = "Valid Syntax"
+    }else {
+        result = "Invalid Syntax"
+    }
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
